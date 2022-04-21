bb54b814fa18dad509c714d9f5c3fb60d37aa481
Kirbitz <tyler.marefke@gmail.com>
2022-04-21 09:46:34 -0500

    Added a variety of hand ciphers

--- complexHandCiphers.py
+++ complexHandCiphers.py
@@ -425,14 +425,305 @@ def playfair_decryption(ciphertext, key):
 
 '''Digraph Substitution'''
 
-'''Improving Keywords'''
-'''Improving Keywords'''
+'''Atbash Cipher'''
+def at_bash_encryption(plaintext):
+    # First makes plaintext into all lowercase
+    plaintext = re.sub("[^a-z]", "", plaintext.lower())
+    ciphertext = ""
+    # Loops through the letters in plaintext and flips from from a to z, b to y, c to x, etc.
+    for letter in plaintext:
+        ciphertext += chr(-((ord(letter) - ord('a')) + 1) % 26 + ord('a'))
+    return ciphertext
 
-'''Generating random digits'''
-'''Generating random digits'''
+def at_bash_decryption(ciphertext):
+    # First makes ciphertext into all lowercase
+    ciphertext = re.sub("[^a-z]", "", ciphertext.lower())
+    plaintext = ""
+    # Loops through the letters in ciphertext and flips from from a to z, b to y, c to x, etc.
+    for letter in ciphertext:
+        plaintext += chr(-((ord(letter) - ord('a')) + 1) % 26 + ord('a'))
+    return plaintext
+'''Atbash Cipher'''
 
-'''Super-encryption'''
-'''Super-encryption'''
+'''ROT13 Cipher'''
+def rot13_encryption(plaintext):
+    # First makes plaintext into all lowercase
+    plaintext = re.sub("[^a-z]", "", plaintext.lower())
+    ciphertext = ""
+    # Loops through the letters in plaintext and increments each by 13 mod 26
+    for letter in plaintext:
+        ciphertext += chr(((ord(letter) - ord('a')) + 13) % 26 + ord('a'))
+    return ciphertext
 
-'''Knock-Knock'''
-'''Knock-Knock'''
+def rot13_decryption(ciphertext):
+    # First makes ciphertext into all lowercase
+    ciphertext = re.sub("[^a-z]", "", ciphertext.lower())
+    plaintext = ""
+    # Loops through the letters in ciphertext and decrements each by 13 mod 26
+    for letter in ciphertext:
+        plaintext += chr(((ord(letter) - ord('a')) - 13) % 26 + ord('a'))
+    return plaintext
+'''ROT13 Cipher'''
+
+'''Caesar Cipher'''
+def caesar_encryption(plaintext, shiftVal):
+    # First makes plaintext into all lowercase
+    plaintext = re.sub("[^a-z]", "", plaintext.lower())
+    ciphertext = ""
+    # Loops through the letters in plaintext and increments each by shiftVal mod 26
+    for letter in plaintext:
+        ciphertext += chr(((ord(letter) - ord('a')) + shiftVal) % 26 + ord('a'))
+    return ciphertext
+
+def caesar_decryption(ciphertext, shiftVal):
+    # First makes ciphertext into all lowercase
+    ciphertext = re.sub("[^a-z]", "", ciphertext.lower())
+    plaintext = ""
+    # Loops through the letters in ciphertext and decrements each by shiftVal mod 26
+    for letter in ciphertext:
+        plaintext += chr(((ord(letter) - ord('a')) - shiftVal) % 26 + ord('a'))
+    return plaintext
+'''Caesar Cipher'''
+
+'''Affine Cipher'''
+def gcd_check(a, b):
+    # Euclidean Alg that returns true if a and b are relatively prime otherwise it returns false
+    i = -1
+    while i != 0:
+        i = b % a
+        b = a
+        a = i
+    if b == 1:
+        return True
+    return False
+
+def affine_encryption(plaintext, multVal, shiftVal):
+    # First makes plaintext into all lowercase
+    plaintext = re.sub("[^a-z]", "", plaintext.lower())
+    multVal %= 26
+    # Checks that multVal and 26 are relatively prime to eachother
+    if not gcd_check(multVal, 26):
+        return "Multiply Value entered is not relatively prime to 26.\nPlease enter a number that is relatively prime to 26!"
+    ciphertext = ""
+    # Loops through the letters in ciphertext and multiplies by multVal then increments each by shiftVal mod 26
+    for letter in plaintext:
+        ciphertext += chr((multVal * (ord(letter) - ord('a')) + shiftVal) % 26 + ord('a'))
+    return ciphertext
+
+def affine_decryption(ciphertext, multVal, shiftVal):
+    # First makes ciphertext into all lowercase
+    ciphertext = re.sub("[^a-z]", "", ciphertext.lower())
+    # Takes the inverse of multVal for decryption
+    multVal = -multVal % 26
+    # Checks that multVal and 26 are relatively prime to eachother
+    if not gcd_check(multVal, 26):
+        return "Multiply Value entered is not relatively prime to 26.\nPlease enter a number that is relatively prime to 26!"
+    plaintext = ""
+    # Loops through the letters in plaintext and decrements each by shiftVal then multiplies by multVal mod 26
+    for letter in ciphertext:
+        plaintext += chr((multVal * ((ord(letter) - ord('a')) - shiftVal)) % 26 + ord('a'))
+    return plaintext
+'''Affine Cipher'''
+
+'''Rail-Fence Cipher'''
+def rail_fence_encryption(plaintext, railVal):
+    # First makes plaintext into all lowercase
+    plaintext = re.sub("[^a-z]", "", plaintext.lower())
+    if railVal <= 1 or railVal >= len(plaintext):
+        return plaintext
+
+    # Creates parts for each "rail" in the rail-fence Cipher
+    ciphertextparts = ["" for i in range(railVal)]
+    # variable to determine how much the i value should be incremented on each iteration
+    incrementVal = 2*railVal - 2
+    for i in range(0, len(plaintext), incrementVal):
+        # Appends the top rails to string in pos 0
+        ciphertextparts[0] += plaintext[i]
+        # Appends each char to rails 1 through n-2
+        for j in range(1, railVal - 1):
+            if i+j < len(plaintext):
+                ciphertextparts[j] += plaintext[i+j]
+                if (i+incrementVal)-j < len(plaintext):
+                    ciphertextparts[j] += plaintext[(i+incrementVal)-j]
+        # Appends chars for rail n-1
+        if i + (railVal-1) < len(plaintext):
+            ciphertextparts[railVal-1] += plaintext[i + (railVal-1)]
+    
+    # Joins each ciphertextpart into ciphertext
+    ciphertext = "".join(ciphertextparts)
+    
+    return ciphertext
+
+def rail_fence_decryption(ciphertext, railVal):
+    # First makes ciphertext into all lowercase
+    ciphertext = re.sub("[^a-z]", "", ciphertext.lower())
+    if railVal <= 1 or railVal >= len(ciphertext):
+        return ciphertext
+    
+    # Calculates the value to dived by for parsing
+    parseVal = 2*railVal - 2
+    # Grabs the parts of the plaintext equal to the number of rails
+    plaintextparts = parse_ciphertext(ciphertext, parseVal, railVal)
+    i = 0
+    plaintext = ""
+    # Loops through length of part 1 of ciphertext
+    while i < len(plaintextparts[0]):
+        # Appends the ith letter of the first part to the plaintext
+        plaintext += plaintextparts[0][i:i+1]
+        # Appends ith * 2 letter of parts j through railVal - 2 to the plaintext
+        for j in range(1, railVal - 1):
+            plaintext += plaintextparts[j][i*2:i*2+1]
+        # Appends the ith letter of the last part to the plaintext
+        plaintext += plaintextparts[railVal - 1][i:i+1]
+        # Appends ith * 2 + 1 letter of parts j through railVal - 2 to the plaintext
+        for j in range(railVal - 2, 0, -1):
+            plaintext += plaintextparts[j][i*2+1:i*2+2]
+        i+=1
+    
+    return plaintext
+
+def parse_ciphertext(ciphertext, parseVal, railVal):
+    # Determines how many parts there are based on the railVal
+    plaintextparts = ["" for i in range(railVal)]
+    # Subtracts one from ciphertext length so one letter is removed from the first part
+    ciphertext_length = len(ciphertext) - 1
+    # Calculates how many left over letters there are after the last letter is added to the first row
+    leftoverletters = ciphertext_length % parseVal
+    # Grabs the first part of the ciphertext into plaintext parts by dividing length of ciphertext - 1 by parseVal and adding 1
+    plaintextparts[0] = ciphertext[:ciphertext_length//(parseVal) + 1]
+    # Removes the first part from the ciphertext
+    ciphertext = ciphertext[ciphertext_length//(parseVal) + 1:]
+    # Loops until number of rails - 1
+    for i in range(1, railVal - 1):
+        # Calculates how long the part should be by taking length of ciphertext - 1 and dividing by parseVal then multiplying by 2
+        parseLength = (ciphertext_length//(parseVal)) * 2
+        # Adds 2 to parseLength if parseVal - leftoverletters is equal to row index
+        if parseVal - leftoverletters == i:
+            parseLength += 2
+            leftoverletters -= 1
+        # Adds 1 to parseLength if previous condition failed and lefteroverletters is greater than row index 
+        elif leftoverletters >= i:
+            parseLength += 1
+        
+        # Puts ciphertext from 0 to parseLength into plaintextpart at row index i
+        plaintextparts[i] = ciphertext[:parseLength]
+        # Removes that part from the ciphertext
+        ciphertext = ciphertext[parseLength:]
+    # Puts remaining ciphertext into the last row of plaintextparts
+    plaintextparts[railVal - 1] = ciphertext
+    return plaintextparts
+'''Rail-Fence Cipher'''
+
+'''Baconian Cipher'''
+def baconian_encryption(plaintext):
+    # First converts the plaintext and key to all lowercase letters
+    plaintext = re.sub("[^a-z]", "", plaintext.lower())
+
+    ciphertext = ""
+    # Loops through each letter of plantext
+    for letter in plaintext:
+        # Grabs the value of the letter mod 26
+        letter_val = ord(letter) - ord('a')
+        # Checks if the value is greater than j and v and subtracts 1 for each respectively
+        if ord(letter) >= ord('j'):
+            letter_val -= 1
+        if ord(letter) >= ord('v'):
+            letter_val -= 1
+        # Formates the integer into a 5 bit binary string then replaces 0 with a and 1 with b
+        binary = '{0:05b}'.format(letter_val)
+        binary = binary.replace('0', 'a')
+        binary = binary.replace('1', 'b')
+        ciphertext += binary
+    
+    return ciphertext
+
+def baconian_decryption(ciphertext):
+    # First converts the plaintext and key to all lowercase letters
+    ciphertext = re.sub("[^ab]", "", ciphertext.lower())
+
+    plaintext = ""
+    # Loops through blocks of 5 in ciphertext
+    for i in range(0, len(ciphertext), 5):
+        # Takes a block of 5 from the ciphertext
+        block = ciphertext[i:i+5]
+        # Replaces the a with 0 and b with 1
+        binary = block.replace('a', '0')
+        binary = binary.replace('b', '1')
+        # Converts the binary string into an integer
+        letter_val = int(binary, 2)
+        # Checks if the integer val is greater than j and v and adds 1 for each respectively
+        if letter_val >= ord('j') - ord('a'):
+            letter_val += 1
+        if letter_val >= ord('v') - ord('a'):
+            letter_val += 1
+        plaintext += chr(ord('a') + letter_val)
+    return plaintext
+'''Baconian Cipher'''
+
+'''Autokey Cipher'''
+def autokey_encryption(plaintext, key):
+    # First converts the plaintext and key to all lowercase letters
+    plaintext = re.sub("[^a-z]", "", plaintext.lower())
+    key = re.sub("[^a-z]", "", key.lower())
+    # Appends key with plaintext so the length of key and plaintext match
+    key += plaintext[:len(plaintext)-len(key)]
+    ciphertext = ""
+    # Loops through each letter adding plaintext at i to key at i mod 26
+    for i in range(len(plaintext)):
+        ciphertext += chr((ord(key[i]) + ord(plaintext[i]) - 2*ord('a')) % 26 + ord('a'))
+    
+    return ciphertext
+
+def autokey_decryption(ciphertext, key):
+    # First converts the ciphertext and key to all lowercase letters
+    ciphertext = re.sub("[^a-z]", "", ciphertext.lower())
+    key = re.sub("[^a-z]", "", key.lower())
+    plaintext = ""
+    # Loops through each letter subtracting key at i from ciphertext at i mod 26
+    for i in range(len(ciphertext)):
+        plaintext += chr((ord(ciphertext[i]) - ord(key[i])) % 26 + ord('a')) 
+        # Appends letters to key in order to decrypt properly
+        key += chr((ord(ciphertext[i]) - ord(key[i])) % 26 + ord('a'))
+    
+    return plaintext
+'''Autokey Cipher'''
+
+'''Porta Cipher'''
+def porta_encryption(plaintext, key):
+    # First converts the plaintext and key to all lowercase letters
+    plaintext = re.sub("[^a-z]", "", plaintext.lower())
+    key = re.sub("[^a-z]", "", key.lower())
+    key_length = len(key)
+    ciphertext = ""
+    blocks = [plaintext[i:i+key_length] for i in range(0, len(plaintext), key_length)]
+    for block in blocks:
+        for i in range(len(block)):
+            letter_val = ord(block[i]) - ord('a')
+            letter_val_append = 0
+            if letter_val > 12:
+                letter_val_append = (letter_val - (ord(key[i]) - ord('a')) // 2) % 13 + ord('a')
+            else:
+                letter_val_append = (letter_val + (ord(key[i]) - ord('a')) // 2) % 13 + ord('n')
+            ciphertext += chr(letter_val_append)
+    
+    return ciphertext
+
+def porta_decryption(ciphertext, key):
+    # First converts the plaintext and key to all lowercase letters
+    ciphertext = re.sub("[^a-z]", "", ciphertext.lower())
+    key = re.sub("[^a-z]", "", key.lower())
+    key_length = len(key)
+    plaintext = ""
+    blocks = [ciphertext[i:i+key_length] for i in range(0, len(ciphertext), key_length)]
+    for block in blocks:
+        for i in range(len(block)):
+            letter_val = ord(block[i]) - ord('a')
+            letter_val_append = 0
+            if letter_val > 12:
+                letter_val_append = (letter_val - (ord(key[i]) - ord('a')) // 2) % 13 + ord('a')
+            else:
+                letter_val_append = (letter_val + (ord(key[i]) - ord('a')) // 2) % 13 + ord('n')
+            plaintext += chr(letter_val_append)
+    
+    return plaintext
+'''Porta Cipher'''
