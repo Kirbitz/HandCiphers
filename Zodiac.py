27b604914235a5acd5456024d4d201fa8c49fc30
AndrewUjda <60474260+AndrewUjda@users.noreply.github.com>
2022-04-23 13:53:43 -0500

    Add files via upload

--- Zodiac.py
+++ Zodiac.py
@@ -13,11 +13,25 @@
 def Z340(text):
   # char assigns to be shifted by value of other paired letter.
   # for example, for 'at'   'a' would be shifted in a direction w/ distance of 't' and vice versa
-  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-  'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] #also might add /, ., -, @, &, *
+  textEx = "dabadb"
+  alphabet = ['/', '.', '-', '&', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+  'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  i = 0
+  for i in textEx:
+    temp1 = textEx[i]
+    temp2 = textEx[i+1]
+    i+2
+    # m = find location of temp1 in alphabet array
+    # n = find location of temp2 in alphabet array
+    temp3 = alphabet[(m + n) % 30]
+    temp4 = alphabet[(m - n) % 30]
 
-  #i = 0
-  #j = 0
-  #firstblock = [['' for i in range(17)] for j in range(9)]
-  #firstblock[i+2][j+1] = 'j'
-  #print(firstblock[2][1])
+  l = 0
+  k = 0
+  firstblock = [['' for l in range(17)] for k in range(9)]
+  #write if statement that wraps around 2D array to avoid out of bounds error
+  firstblock[l+2][k+1] = temp3
+  l+2
+  k+1
+  firstblock[l+2][k+1] = temp4
+  #write loop to output all arrays from left to right and up to down
